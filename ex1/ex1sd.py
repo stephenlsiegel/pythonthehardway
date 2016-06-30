726132378895adec42efd296716ba728ad377698
Stephen Siegel <stephenlsiegel@gmail.com>
2016-06-29 17:22:32 -0700

    Add exercise 1

---
+++ ex1/ex1sd.py
@@ -0,0 +1,18 @@
+# Study Drills
+
+# (1) Make your script print another line.
+print "Here is another line."
+
+# (2) Make your script print only one of the lines.
+
+# print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+
+# (3) Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+# The octothorpe (#) is the comment symbol. Code on a line after the # won't run.
