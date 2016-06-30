e7fd6c1f747584883369000e11c0bc572a68b8ad
Stephen Siegel <stephenlsiegel@gmail.com>
2016-06-29 21:33:52 -0700

    Add exercise 2

---
+++ ex2/ex2.py
@@ -0,0 +1,8 @@
+# Comments are used to add notes or to disable parts of a program
+# Anything after the # is a comment
+
+print "I could have code like this." # and the comment after is ignored
+
+# print "This won't run."
+
+print "This will run."
