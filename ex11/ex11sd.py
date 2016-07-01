b42f5b365cc854c309abe7b8bb05fa7642f3c960
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-01 14:31:14 -0700

    Add exercise 11

---
+++ ex11/ex11sd.py
@@ -0,0 +1,21 @@
+# # Asking questions
+
+# # Most of what software does is the following:
+# # 	1. Take some kind of input from a person.
+# # 	2. Change it.
+# # 	3. Print out something to show how it changed.
+
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weigh?",
+# weight = raw_input()
+
+# print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# STUDY DRILLS
+
+print "What is your name?"
+name = raw_input("--> ")
+print "Hello, %s." % name
