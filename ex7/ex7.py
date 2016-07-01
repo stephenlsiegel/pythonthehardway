b9c5367ca9859c0bf155e3bd16acca1f8fd969c4
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-01 13:35:45 -0700

    Add exercise 7

---
+++ ex7/ex7.py
@@ -0,0 +1,30 @@
+# More printing
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s" % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what does this do? It prints the period 10 times without spaces.
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch the comma at the end. Try removing it to see what happens.
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# The comma at the end of line 23 causes line 23 and line 24 to print on the same line with a space
+
