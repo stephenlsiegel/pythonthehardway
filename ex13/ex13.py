79105590a31bb7cf5ceafc2e1f146ef6cba63693
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-01 15:20:11 -0700

    Add exercise 13

---
+++ ex13/ex13.py
@@ -0,0 +1,14 @@
+# Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# Note: to run this, you must pass three command line arguments:
+
+# python ex13/ex13.py first 2nd 3rd
