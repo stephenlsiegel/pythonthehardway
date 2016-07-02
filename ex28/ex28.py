5c58378b147edba8cd80781c2d3235ec2a3d98fe
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-02 16:59:56 -0700

    Add exercise 28

---
+++ ex28/ex28.py
@@ -0,0 +1,86 @@
+# Boolean Practice
+
+# 1. True
+# 2. False
+# 3. False
+# 4. True
+# 5. True
+# 6. True
+# 7. False
+# 8. True
+# 9. False
+# 10. False
+# 11. True
+# 12. False
+# 13. True
+# 14. False
+# 15. False
+# 16. False
+# 17. True
+# 18. True
+# 19. False
+# 20. False
+
+# Got all of these on the first try...I'm pretty comfortable with boolean logic.
+
+i = 1
+
+print i, True and True
+i += 1
+
+print i, False and True
+i += 1
+
+print i, 1 == 1 and 2 == 1
+i += 1
+
+print i, "test" == "test"
+i += 1
+
+print i, 1 == 1 or 2 != 1
+i += 1
+
+print i, True and 1 == 1
+i += 1
+
+print i, False and 0 != 0
+i += 1
+
+print i, True or 1 == 1
+i += 1
+
+print i, "test" == "testing"
+i += 1
+
+print i, 1 != 0 and 2 == 1
+i += 1
+
+print i, "test" != "testing"
+i += 1
+
+print i, "test" == 1
+i += 1
+
+print i, not (True and False)
+i += 1
+
+print i, not (1 == 1 and 0 != 1)
+i += 1
+
+print i, not (10 == 1 or 1000 == 1000)
+i += 1
+
+print i, not (1 != 10 or 3 == 4)
+i += 1
+
+print i, not ("testing" == "testing" and "Zed" == "Cool Guy")
+i += 1
+
+print i, 1 == 1 and (not ("testing" == 1 or 1 == 0))
+i += 1
+
+print i, "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+i += 1
+
+print i, 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+i += 1
