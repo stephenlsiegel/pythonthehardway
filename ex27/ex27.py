99050c5afb602da43739b7563d5f33d474b1c125
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-02 16:48:34 -0700

    Add exercise 27

---
+++ ex27/ex27.py
@@ -0,0 +1,56 @@
+# Memorizing Logic
+
+# The Truth Terms
+
+# and
+# or
+# not
+# != (not equal)
+# ==
+# >=
+# <=
+# True
+# False
+
+
+# The Truth Tables
+
+########## not ##########
+# not False evaluates to True
+# not True evaluates to False
+
+########## or ##########
+# True or False === True
+# True or True === True
+# False or True === True
+# False or False === False
+
+########## and ##########
+# True and False === False
+# True and True === True
+# False and True === False
+# False and False === False
+
+########## not or ##########
+# not (True or False) === False
+# not (True or True) === False
+# not (False or True) === False
+# not (False or False) === True
+
+########## not and ##########
+not (True and False) === True
+not (True and True) === False
+not (False and True) ===  True
+not (False and False) === True
+
+########## != ##########
+# 1 != 0 === True
+# 1 != 1 === False
+# 0 != 1 === True
+# 0 != 0 === False
+
+########## == ##########
+# 1 == 0 === False
+# 1 == 1 === True
+# 0 == 1 === False
+# 0 == 0 === True
