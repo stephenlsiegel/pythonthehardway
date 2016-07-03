4a6a66347145c3de817bbeea60850f79670e5f8f
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-03 11:11:53 -0700

    Add exercise 34

---
+++ ex34/ex34.py
@@ -0,0 +1,12 @@
+# Accessing Elements of Lists
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+# Python starts lists at index 0!
+
+# Let's practice:
+
+# ordinal >>  1        2         3           4         5         6
+# cardinal >> 0        1         2           3         4         5
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
