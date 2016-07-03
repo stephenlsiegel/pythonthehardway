e8952e2f284c98984a3cac472b501a1dfdc574d8
Stephen Siegel <stephenlsiegel@gmail.com>
2016-07-03 12:40:54 -0700

    Add exercise 37

---
+++ ex37/ex37.py
@@ -0,0 +1,61 @@
+# Symbol Review
+
+# Keywords we've learned:
+	# and
+	# break
+	# def
+	# elif
+	# else
+	# for
+	# from
+	# if
+	# import
+	# in
+	# not
+	# or
+	# print
+	# return
+	# while
+
+# Keywords included in this exercise but learned yet:
+	# as
+	# assert
+	# class
+	# continue
+	# del
+	# except
+	# exec
+	# finally
+	# global
+	# is
+	# lambda
+	# pass
+	# raise
+	# try
+	# with
+	# yield
+
+# Data Types we've learned
+	# True
+	# False
+	# None
+	# strings
+	# numbers
+	# floats
+	# lists
+
+# Data Types we haven't learned:
+	# dicts
+	
+# String escape sequences:
+	# \\ backslash
+	# \' single quote
+	# \" double quote
+	# \a bell
+	# \b backspace
+	# \f formfeed
+	# \n newline
+	# \r carriage
+	# \t tab
+	# \v vertical tab
+	
